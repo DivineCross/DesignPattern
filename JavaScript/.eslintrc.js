f4c536ea80839a883e061bd3e7da73d3ed8b80d5
DivineCross <DivineCross@users.noreply.github.com>
2020-03-15 12:43:38 +0800

    Install 'eslint'

---
+++ JavaScript/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'esnext',
+        'airbnb-base',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        'arrow-parens': ['error', 'as-needed'],
+        'class-methods-use-this': 'off',
+        'curly': ['error', 'multi'],
+        'import/extensions': ['error', 'ignorePackages', {
+            'js': 'always',
+        }],
+        'import/no-default-export': 'error',
+        'import/prefer-default-export': 'off',
+        'indent': ['error', 4],
+        'new-cap': ['error', { 'newIsCap': false }],
+        'new-parens': ['error', 'never'],
+        'nonblock-statement-body-position': ['error', 'below'],
+        'no-console': 'off',
+        'no-plusplus': 'off',
+        'no-var': 'error',
+    },
+};
