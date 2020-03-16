4b5134d06a1074225c34ab9e42842f8fd0d5681a
DivineCross <DivineCross@users.noreply.github.com>
2020-03-16 17:29:56 +0800

    Reinstall and setup tools for ts

--- JavaScript/.eslintrc.js
+++ JavaScript/.eslintrc.js
@@ -4,33 +4,29 @@ module.exports = {
         node: true,
     },
     extends: [
-        'esnext',
-        'airbnb-base',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
     ],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
+    plugins: [
+        '@typescript-eslint',
+    ],
     rules: {
-        'arrow-parens': ['error', 'as-needed'],
-        'class-methods-use-this': 'off',
-        'curly': ['error', 'multi'],
-        'import/extensions': ['error', 'ignorePackages', {
-            'js': 'always',
-        }],
-        'import/no-default-export': 'error',
-        'import/prefer-default-export': 'off',
         'indent': ['error', 4],
-        'new-cap': ['error', { 'newIsCap': false }],
-        'new-parens': ['error', 'never'],
-        'nonblock-statement-body-position': ['error', 'below'],
-        'no-console': 'off',
-        'no-plusplus': 'off',
-        'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+        'linebreak-style': ['error', 'unix'],
+        'no-unused-vars': ['error', {
+            'varsIgnorePattern': '^I[A-Z]',
+        }],
         'no-var': 'error',
-    },
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+    }
 };
